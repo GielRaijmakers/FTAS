4b9db2ae8f8a2d7be24c59607136059c499517fd
GielRaijmakers <graijmakers@home.nl>
2015-06-18 10:06:50 +0200

    first commit repository
    generic repository function. adding image path to the project.

---
+++ repository.sikuli/repository.rb
@@ -0,0 +1,11 @@
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#
+#repository functions for FTAS
+#Giel Raijmakers
+#Version 0.1
+# https://github.com/GielRaijmakers/FTAS
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#
+
+
+#add path to the project
+addImagePath('../generic.sikuli')
+addImagePath('../project.sikuli')
